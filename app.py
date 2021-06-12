ab1fb7ae3facb36fd7d10e8a1619278d1dfba250
Mayank Shrivastava <mayanksri910@gmail.com>
2021-06-12 16:36:08 +0530

    Added comment to app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+# using flask as backend 
 from flask import Flask, render_template, request
+# using autocorrect module to get correct spelling  
 from autocorrect import Speller
 
+# setting the default language as english 
 spell = Speller(lang='en')
+# define flask 
 app = Flask(__name__)
 
+# home page 
 @app.route('/')
 def show_home():
     return render_template('index.html')
 
+# to get the correct spelling using autocorrect 
 @app.route('/get')
 def show_result():
     userText = request.args.get('msg')
